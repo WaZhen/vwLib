7103a22df9408a2c7c6187bcd1b508e6cfd42136
wazhen <wangzhenou@gmail.com>
2020-12-08 16:25:55 +0100

    Add VwRequest docs

--- src/Networking/VwRequest.js
+++ src/Networking/VwRequest.js
@@ -1,4 +1,19 @@
+/**
+ * Class for using XmlHttpRequest
+ */
 export default class VwRequest {
+    /**
+     * 
+     * @param {string} [method=GET] - String with the http method
+     * @param {object} [data=undefined] - Required for POST/PUT/DELETE/PATCH methods
+     * @param {boolean} [async=true]
+     * @param {string} [contentType=application/json; charset=UTF-8] - Override if other content type
+     * @param {string} [headers={}] - Additional request headers
+     * @param {int} [timeout=5000] - Max waiting time in miliseconds
+     * @param {function} success - Function to call if success
+     * @param {function} error - Function to call if unsuccessful response
+     * @param {string} url - request endpoint
+     */
     static send({
         method = "GET",
         data = undefined,
