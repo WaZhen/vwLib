f1ad452f218ae0a82dea82a4ed73c24e30d9d785
jwang <jwang@visualms.com>
2022-04-25 15:18:13 +0200

    VwObjets

--- src/ObjectsAPI/VwObjects/VwObjects.js
+++ src/ObjectsAPI/VwObjects/VwObjects.js
@@ -1,7 +1,15 @@
 /**
- * Class for use VProjectInfo methods {@link https://doc.velneo.es/vprojectinfo.html|VProjectInfo}
+ * @classdesc
+ * Utilidades para {@link https://doc.velneo.es/vprojectinfo.html|VProjectInfo}
+ * @hideconstructor
  */
 export default class VwObjects {
+    /**
+     * Obtiene los procesos de un proyecto
+     * @param {string} projectAlias alias del proyecto
+     * @returns {string[]} Array con los idRef de los procesos
+     * @method
+     */
     static getProcessArray = (projectAlias) => {
         const projectInfo = theApp.projectInfo(projectAlias)
         const processQuantity = projectInfo.objectCount(VObjectInfo.TypeProcess);
@@ -14,19 +22,36 @@ export default class VwObjects {
         return processArray;
     }
 
-    static getProcessRegisterInTableInfo(projectName, processId) {
-        const proyectInfo = theApp.projectInfo(projectName);
+    /**
+     * Obtiene la [VTableInfo]{@link https://doc.velneo.com/velneo-vdevelop/scripts/lenguajes/javascript/clases/vtableinfo} del registro de origen de un proceso origen ficha
+     * @param {string} projectAlias alias proyecto
+     * @param {string} processId idProceso
+     * @returns {object} [VTableInfo]{@link https://doc.velneo.com/velneo-vdevelop/scripts/lenguajes/javascript/clases/vtableinfo}
+     */
+    static getProcessRegisterInTableInfo(projectAlias, processId) {
+        const proyectInfo = theApp.projectInfo(projectAlias);
         const process = proyectInfo.objectInfo(VObjectInfo.TypeProcess, processId);
         const tableInfo = process.inputTable();
         return tableInfo;
     }
 
+    /**
+     * Devuelve el [VObjectInfo]{@link https://doc.velneo.com/velneo-vdevelop/scripts/lenguajes/javascript/clases/vobjectinfo} de un proceso
+     * @param {string} projectAlias Alias del proyecto
+     * @param {*} processId idProceso
+     * @returns {object} [VObjectInfo]{@link https://doc.velneo.com/velneo-vdevelop/scripts/lenguajes/javascript/clases/vobjectinfo}
+     */
     static getProcessObjectInfo(projectAlias, processId) {
         const proyectInfo = theApp.projectInfo(projectAlias);
         const processInfo = proyectInfo.objectInfo(VObjectInfo.TypeProcess, processId);
         return processInfo
     }
 
+    /**
+     * Duevuelve un array de VObjectInfo de procesos de un proyecto
+     * @param {string} projectAlias Alias del proyecto
+     * @returns {object[]} Devuelve un array de [VObjectInfo]{@link https://doc.velneo.com/velneo-vdevelop/scripts/lenguajes/javascript/clases/vobjectinfo}
+     */
     static getProcessObjectInfoArray(projectAlias) {
         const processIdRefArray = VwObjects.getProcessArray(projectAlias);
         return processIdRefArray.map((processIdRef) => {
